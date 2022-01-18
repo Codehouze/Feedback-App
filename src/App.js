aeb5e459d1ee56cc90d197c622c6a3cd36a1569b
Codehouzem <codehouseinc@gmail.com>
2022-01-18 01:44:28 +0100

    added rating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react";
 import Header from "./Components/Header";
 import FeedbackList from "./Components/FeedbackList";
-import FeedbackData from "./data/FeedbackData"
-import FeedbackStats from "./Components/FeedbackStats"
-import FeedbackForm from './Components/FeedbackForm';
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./Components/FeedbackStats";
+import FeedbackForm from "./Components/FeedbackForm";
 
 function App() {
-  const[feedback, setFeedback]=useState(FeedbackData)
-  const deleteFeedback =(id)=>{
-    if(window.confirm('Are you sure you want to delete ?')){
-      setFeedback(feedback.filter((item) => item.id!==id))
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete ?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-  }
- return (
+  };
+  return (
     <>
-    <Header   />
-    <FeedbackForm />
-    <FeedbackStats feedback={feedback} />
-    <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-    
+      <Header />
+      <FeedbackForm />
+      <FeedbackStats feedback={feedback} />
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      <div className="footer">
+        <h2>
+          Powered by<a href="https://github.com/Codehouze"> Codehouze</a>
+        </h2>
+      </div>
     </>
- )
+  );
 }
 
 export default App;
--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,27 +1,58 @@
- import {useState} from 'react'
- import Card from './shared/Card'
+import { useState } from "react";
+import RatingSelect from "./RatingSelect";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
 function FeedbackForm() {
-    const [text,setText] =useState('text');
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [message, setMessage] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
 
-    const handleTextChange =(e)=>{
-        setText(e.target.value) 
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text Must Be at least 10 characters long");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
     }
-    return (
-        <Card>
-            <form >
-                <h2>How would you rate your service with us?</h2>
-                {
-                // @todo - rating select components 
-                <div className="input-group">
-                    <input 
-                    onChange={handleTextChange}
-                    type="text" placeholder = "Write a review"  value={text}/>
-                    <button type="submit">Send</button>
-                </div>
-                }
-            </form>
-        </Card>
-    )
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      console.log(newFeedback);
+    }
+  };
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
 }
 
-export default FeedbackForm
+export default FeedbackForm;
